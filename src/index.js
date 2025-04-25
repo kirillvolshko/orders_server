100c2a354bcc703dfeaee56846c7e93dccabb116
kyrylovoloshko <k.voloshko04@gmail.com>
2025-04-25 22:28:40 +0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import errorMiddleware from "./middlewares/error-middleware";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
+app.use(errorMiddleware);
 const startApp = async () => {
   try {
     app.listen(PORT, () => console.log("START SERVER"));
